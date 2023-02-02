4f2489a946be32476f8f48262c46c9538f978eb6
MyeongQ <gmk0904@ajou.ac.kr>
2023-02-02 22:06:49 +0900

    Spring Container 방식으로 변경

--- buildName/src/main/java/my_shopping_system/buildName/AppConfig.java
+++ buildName/src/main/java/my_shopping_system/buildName/AppConfig.java
@@ -2,32 +2,41 @@ package my_shopping_system.buildName;
 
 import my_shopping_system.buildName.discount.DiscountPolicy;
 import my_shopping_system.buildName.discount.FixDiscountPolicy;
+import my_shopping_system.buildName.discount.RateDIscountPolicy;
 import my_shopping_system.buildName.member.MemberRepository;
 import my_shopping_system.buildName.member.MemberService;
 import my_shopping_system.buildName.member.MemberServiceImp;
 import my_shopping_system.buildName.member.MemoryMemberRepository;
 import my_shopping_system.buildName.order.OrderService;
 import my_shopping_system.buildName.order.OrderServiceImp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 어플리케이션 동작에 필요한 구현 객체를 생성 -> 주입
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImp(memberRepository()
         );
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImp(
                 memberRepository(),
                 discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        //return new FixDiscountPolicy();
+        return new RateDIscountPolicy();
     }
 }
--- buildName/src/main/java/my_shopping_system/buildName/OrderApp.java
+++ buildName/src/main/java/my_shopping_system/buildName/OrderApp.java
@@ -1,27 +1,35 @@
 package my_shopping_system.buildName;
 
+import my_shopping_system.buildName.AppConfig;
 import my_shopping_system.buildName.member.Grade;
 import my_shopping_system.buildName.member.Member;
 import my_shopping_system.buildName.member.MemberService;
 import my_shopping_system.buildName.member.MemberServiceImp;
+import my_shopping_system.buildName.order.Order;
+import my_shopping_system.buildName.order.OrderService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class MemberApp {
+public class OrderApp {
 
-    // psvm = public static void main
     public static void main(String[] args) {
+
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
+        //OrderService orderService = new OrderServiceImp();
+//        OrderService orderService = appConfig.orderService();
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
         
-        Member findMember = memberService.findMember(1L);
-        System.out.println("findMember = " + member.getName());
-        System.out.println("findMember = " + findMember.getName());
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
+
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice() = " + order.calculatePrice());
     }
 }
