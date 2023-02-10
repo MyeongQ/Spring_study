ec50a692921e6b33b64f0a2ca9b94b1dfec00551
MyeongQ <gmk0904@ajou.ac.kr>
2023-02-10 18:32:49 +0900

    스프링 컨테이너 싱글톤 + stateful Service 구현

--- buildName/src/test/java/my_shopping_system/buildName/singleton/StatefulService.java
+++ buildName/src/test/java/my_shopping_system/buildName/singleton/StatefulService.java
@@ -1,2 +1,15 @@
-package my_shopping_system.buildName.singleton;public class StatefulService {
+package my_shopping_system.buildName.singleton;
+
+public class StatefulService {
+
+    private int price; // 상태를 유지하는 필드
+
+    public void order(String name, int price) {
+        System.out.println("name = " + name + " price =  " + price);
+        this.price = price;  // 여기가 문제!
+    }
+    public int getPrice() {
+        return price;
+    }
+
 }
