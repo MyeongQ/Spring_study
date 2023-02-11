6e3d84e39d85975484b63cfe30caf5d51f844ef4
MyeongQ <gmk0904@ajou.ac.kr>
2023-02-11 19:17:25 +0900

    빈 2개 충돌 테스트

--- buildName/src/main/java/my_shopping_system/buildName/discount/FixDiscountPolicy.java
+++ buildName/src/main/java/my_shopping_system/buildName/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package my_shopping_system.buildName.discount;
 
 import my_shopping_system.buildName.member.Grade;
 import my_shopping_system.buildName.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;  // 1,000원 할인
