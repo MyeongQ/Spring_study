01c9b38b481107a80aa0242c1953e2240c32dad8
MyeongQ <gmk0904@ajou.ac.kr>
2023-02-11 16:54:31 +0900

    컴포턴트 탐색 위치 실습

--- buildName/src/main/java/my_shopping_system/buildName/AutoAppConfig.java
+++ buildName/src/main/java/my_shopping_system/buildName/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "my_shopping_system.buildName", // 컴포넌트 탐색 시작 위치
+        basePackageClasses = AutoAppConfig.class, // 컴포넌트 탐색 시작 위치 = 상단 package와 같음
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // AppConfig나 TestConfig 등 @Configuration이 붙은 설정 정보는 제외
 public class AutoAppConfig {
