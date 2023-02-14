9bdb7d526c2a447d1d38b035704814361ca7bab8
MyeongQ <gmk0904@ajou.ac.kr>
2023-02-14 20:57:51 +0900

    매번 컨테이너에 요청하는 방식 (권장 X)

--- buildName/src/test/java/my_shopping_system/buildName/scope/SingletonWithPrototypeTest1.java
+++ buildName/src/test/java/my_shopping_system/buildName/scope/SingletonWithPrototypeTest1.java
@@ -5,6 +5,7 @@ import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -36,20 +37,26 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean1.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입
+        //private final PrototypeBean prototypeBean; // 생성 시점에 주입
+
+        @Autowired
+        private ApplicationContext ac;
+/*
 
         @Autowired
         public ClientBean(PrototypeBean prototypeBean) {
             this.prototypeBean = prototypeBean;
         }
+*/
 
         public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
