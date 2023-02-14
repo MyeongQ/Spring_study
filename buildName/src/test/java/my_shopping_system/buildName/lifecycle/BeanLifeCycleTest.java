c2918a62e2515127de5b1058c5e1894642f09c68
MyeongQ <gmk0904@ajou.ac.kr>
2023-02-14 19:01:25 +0900

    객체 생성 단계 - NetworkClient

---
+++ buildName/src/test/java/my_shopping_system/buildName/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package my_shopping_system.buildName.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        // close()는 기본 ApplicationContext에서는 호출되지 않는다.
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
