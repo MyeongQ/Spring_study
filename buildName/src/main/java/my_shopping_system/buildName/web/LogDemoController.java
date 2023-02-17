ad5b615eee1e54b57d46fba713a34b4b5c7ad799
MyeongQ <gmk0904@ajou.ac.kr>
2023-02-17 15:15:06 +0900

    Provide를 활용한 request 스코프 빈 활용 실습

---
+++ buildName/src/main/java/my_shopping_system/buildName/web/LogDemoController.java
@@ -0,0 +1,32 @@
+package my_shopping_system.buildName.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import my_shopping_system.buildName.common.MyLogger;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    //@Autowired 생략
+    private final LogDemoService logDemoService;
+    // private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+
+}
