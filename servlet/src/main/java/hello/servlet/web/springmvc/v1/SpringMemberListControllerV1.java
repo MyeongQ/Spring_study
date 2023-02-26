8bf638da63228807324fdd0eaf6be8d1723f0798
MyeongQ <gmk0904@ajou.ac.kr>
2023-02-26 17:12:31 +0900

    SpringMVC 적용

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,32 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+    private Map<String, ControllerV3> controllerMap = new HashMap<>();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.getModel().put("members", members);
+
+        return mv;
+    }
+
+}
