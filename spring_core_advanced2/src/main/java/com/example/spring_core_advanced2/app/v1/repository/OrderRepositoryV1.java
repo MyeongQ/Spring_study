e0374f8bd3a9669eb5f317847139bd15aad984ac
MyeongQ <gmk0904@ajou.ac.kr>
2023-08-27 18:22:50 +0900

    tservice, repository Trace 적용

--- spring_core_advanced2/src/main/java/com/example/spring_core_advanced2/app/v1/repository/OrderRepositoryV1.java
+++ spring_core_advanced2/src/main/java/com/example/spring_core_advanced2/app/v1/repository/OrderRepositoryV1.java
@@ -1,18 +1,31 @@
 package com.example.spring_core_advanced2.app.v1.repository;
 
+import com.example.spring_core_advanced2.trace.TraceStatus;
+import com.example.spring_core_advanced2.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderRepositoryV1 {
+    private final HelloTraceV1 trace;
 
     public void save(String itemId) {
-        // 저장 로직
-        if (itemId.equals("ex")) {
-            throw new IllegalStateException("예외 발생!");
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderRepository.save()");
+            // 저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
         }
-        sleep(1000);
+
     }
 
     private void sleep(int millis) {
--- spring_core_advanced2/src/main/java/com/example/spring_core_advanced2/app/v1/service/OrderServiceV1.java
+++ spring_core_advanced2/src/main/java/com/example/spring_core_advanced2/app/v1/service/OrderServiceV1.java
@@ -1,6 +1,8 @@
 package com.example.spring_core_advanced2.app.v1.service;
 
 import com.example.spring_core_advanced2.app.v1.repository.OrderRepositoryV1;
+import com.example.spring_core_advanced2.trace.TraceStatus;
+import com.example.spring_core_advanced2.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +11,20 @@ import org.springframework.stereotype.Service;
 public class OrderServiceV1 {
 
     private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
 
     public void orderItem(String itemId) {
-        orderRepository.save(itemId);
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+
     }
 }
