1935de1ae180819d552749c3d5ff8dae8c340044
MyeongQ <gmk0904@ajou.ac.kr>
2023-08-27 18:02:46 +0900

    Trace 추적기 v0 테스트 성고

--- spring_core_advanced2/src/test/java/com/example/spring_core_advanced2/trace/hellotrace/HelloTraceV1Test.java
+++ spring_core_advanced2/src/test/java/com/example/spring_core_advanced2/trace/hellotrace/HelloTraceV1Test.java
@@ -1,5 +1,21 @@
 package com.example.spring_core_advanced2.trace.hellotrace;
 
+import com.example.spring_core_advanced2.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
 public class HelloTraceV1Test {
 
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
 }
